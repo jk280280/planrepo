a0c31165620895d731de8063bc942eb04ff9262a
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-20 11:17:53 +0530

    Update App.java

--- src/main/java/com/plan/App.java
+++ src/main/java/com/plan/App.java
@@ -1,13 +1,11 @@
-package com.plan;
+package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
     }
 }
